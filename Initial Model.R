fe5dda40b8f291176c6678063a55c8466c43aa97
4r5i <ratogmoy@up.edu.ph>
2023-06-04 23:27:56 +0800

    Update Initial Model.R

--- Initial Model.R
+++ Initial Model.R
@@ -1,40 +1,80 @@
 library(readxl)
 library(olsrr)
+library(tidyr)
+library(car)
+library(lmtest)
 
 #Importing the entire data set
 init.data <- read_xlsx("FinalDataSet_AllVar.xlsx")
 str(init.data)
 
-#Getting only the numeric data needed for modelling
-init.data_mod <- init.data
-str(init.data_mod)
-
-#Making all Columns numeric
-init.data_mod$assets2018 <- as.numeric(init.data_mod$assets2018)
-init.data_mod$liab2018 <- as.numeric(init.data_mod$liab2018)
-init.data_mod$rev2018 <- as.numeric(init.data_mod$rev2018)
-init.data_mod$exp2018 <- as.numeric(init.data_mod$exp2018)
-str(init.data_mod)
-View(init.data_mod)
-
+#---------------------------------------------------------
 #Removing all observations with NAs
-data_mod <- na.omit(init.data_mod)
+data_mod <- na.omit(init.data)
 str(data_mod)
 View(data_mod)
 
-#Fitting initial model
-i.mod <- lm(perc_votes_2019~.-region-province-geog_level-city_municip-assets2018-liab2018-rev2018-exp2018 
-              -temp_ave_perc_chg-maxtemp_ave_prec_chg, data = data_mod)
-summary(i.mod)
-plot(i.mod)
-round(cor(data_mod[,-c(1,2,3,4,5,7,9,11,13)]),2)
+#Fitting initial model with 17 Predictors
+i.mod <- lm(perc_votes_2019~perc_votes_2016
+                +assets_ave_perc_chg
+                +liab_ave_perc_chg
+                +rev_ave_perc_chg
+                +exp_ave_perc_chg
+                +pi_chg
+                +co2_ave_perc_chg
+                +hum_ave_perc_chg
+                +prec_ave_perc_chg
+                +precmax_ave_prec_chg
+                +temp1_ave_perc_chg
+                +maxtemp_ave_prec_chg
+                +factor(ruling_party)
+                +factor(sex)
+                +factor(case_inv)
+                +factor(executive)
+                +factor(legislative)
+               ,data = data_mod)
+
+
+#Model Adequacy
+summary(i.mod) #R^2_a = 0.3522
+anova(i.mod) #MSE = 142.3
+
 
 #Variable Selection
-plot(ols_step_forward_p(i.mod))
+ols_step_forward_p(i.mod) #R_a^2 = 0.3596
+ols_step_backward_p(i.mod)
+ols_step_both_p(i.mod)
+
+#Reduced Model
+i.mod.red <- lm(perc_votes_2019~
+            +perc_votes_2016
+            +hum_ave_perc_chg 
+            +factor(executive)
+            +co2_ave_perc_chg 
+            +factor(legislative)
+            +assets_ave_perc_chg
+            +factor(case_inv)
+            +rev_ave_perc_chg
+            +factor(sex)  
+            ,data = data_mod)
+summary(i.mod.red)
+anova(i.mod.red)
+par(mfrow = c(2,2))
+plot(i.mod.red)
+
+#Diagnostic Analysis
+ols_test_normality(i.mod.red)
+ols_test_breusch_pagan(i.mod.red)
+bptest(i.mod.red, studentize = FALSE)
+
+ols_leverage(i.mod.red)
+
+ols_plot_added_variable(i.mod.red)
+ols_plot_comp_plus_resid(i.mod.red)
 
 
-plot(data_mod$assets_ave_perc_chg,data_mod$perc_votes_2019)
-par(mfrow=c(2,2))
-plot(i.mod)
-View(data_mod[c(171, 166,11),])
+bc<-boxCox(i.mod.red)
+par(mfrow=c(1,1))
+(bc.power <- bc$x[which.max(bc$y)])
 
+#------------------------------------------------------------
